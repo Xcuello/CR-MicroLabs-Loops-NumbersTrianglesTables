ee6cb9fbf336b82b838ae7b0fdd0e41425344ee4
Xcuello <Xzaviacuello@gmail.com>
2019-01-01 21:52:36 -0500

    new changes

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -3,16 +3,32 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
 
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
 
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
 
-        return null;
+        StringBuilder str = new StringBuilder();
+
+        int rowNum;
+        int currentNum;
+
+        for (int i = 0; i < tableSize; i++) {
+            rowNum = i + 1;
+            currentNum = 0;
+
+            for (int j = 0; j < tableSize; j++) {
+
+                currentNum += rowNum;
+                str.append(String.format("%3s |", currentNum));
+            }
+            str.append("\n");
+        }
+        return str.toString();
     }
 }
